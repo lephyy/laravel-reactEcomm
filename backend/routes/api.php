b935e433de7528a67df311e73644d3adf30638d2
Garfield239 <ansreynich23@gmail.com>
2025-04-13 22:46:31 +0700

    api categories and brands

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\admin\AuthController;
+use App\Http\Controllers\admin\BrandController;
+use App\Http\Controllers\admin\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +15,11 @@ Route::middleware('auth:api')->group(function () {
     Route::post('refresh', [AuthController::class, 'refresh']);
 });
 
+// Route::get('categories',[CategoryController::class,'index']);
+// Route::get('categories/{id}',[CategoryController::class,'show']);
+// Route::put('categories/{id}',[CategoryController::class,'update']);
+// Route::delete('categories/{id}',[CategoryController::class,'destroy']);
+// Route::post('categories',[CategoryController::class,'store']);
+
+Route::resource('categories',CategoryController::class);
+Route::resource('brands',BrandController::class);
